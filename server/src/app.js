c8d3a1264c6a438005269c1358f3cade83a2878e
Dendy-D <daniilvasilenko2@gmail.com>
2023-12-02 17:33:14 +0300

    set up server

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,7 @@ import statementParser from './transactionParser.js';
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+const SERVER_IP = process.env.SERVER_IP || 'localhost';
 
 app.use(cors());
 app.use(express.json());
@@ -22,7 +23,7 @@ const start = () => {
     app.post('/api/statement', upload.single('file'), statementParser);
     
     app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
+      console.log(`Server is running on http://${SERVER_IP}:${PORT}`);
     });
   } catch (e) {
     console.error(e);
